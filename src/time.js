5c48e1229607b27a5eb689208eaea6d1031891e9
marcotduenas <marcotulio.dev@gmail.com>
2021-04-02 18:54:37 -0300

    First Version

---
+++ src/time.js
@@ -0,0 +1,30 @@
+setInterval(displayClock, 500);
+function displayClock(){
+    let time = new Date();
+    let h = time.getHours();
+    let m  = time.getMinutes();
+    let s = time.getSeconds();
+    
+    if(h > 12){
+        h = h - 12;
+    }
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h < 10){
+        h = '0' + h;
+    }
+    
+    if(m < 10){
+        m = '0' + m;
+    }
+
+    if(s < 10){
+        s = '0' + s;
+    }
+
+    document.getElementById('clock').innerHTML = h + ':' + m + ':' + s;
+
+}
