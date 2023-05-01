38be56646d3032a9554da99e0203992bb40ae967
Marco T <marcot.dns@gmail.com>
2023-05-01 13:53:36 -0300

    Starting again
    After a lot of procrastination I've finally started this portofolio again... Everything new as it should be

---
+++ js/copyright.js
@@ -0,0 +1,7 @@
+function copyright(){
+    const currentYear = new Date().getFullYear();
+    document.getElementById('copyright').innerText = `\u00A9 ${currentYear} Marco Tulio Dueñas (This is not a company). All rights reserved.`;
+}
+
+copyright();
+
